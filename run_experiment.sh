3fd56a996a84da41a8d0b3ff643caade38fde441
Daniel Swanson <popcorn.tomato.dude@gmail.com>
2020-12-03 16:54:52 -0600

    print time for progress tracking

--- run_experiment.sh
+++ run_experiment.sh
@@ -3,11 +3,13 @@
 for args in {50,100,200}\ {0.05,0.10,0.15,0.20}
 do
   echo $args
+  date
   time ./run_iteration.sh 1000 $args > "results_$args.tsv"
 done
 
 for args in {0.05,0.10,0.15,0.20}
 do
   echo $args
+  date
   time ./run_iteration.sh 10000 $args > "results_$args.tsv"
 done
